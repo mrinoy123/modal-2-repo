7c160b5d3a7a4d576cd5db4747fb9a00e503f35f
Mrinoy Mitra <mrinoycool@gmail.com>
2026-05-29 00:51:32 +0600

    Update LTX-19b-new-v20.py

--- LTX-19b-new-v20.py
+++ LTX-19b-new-v20.py
@@ -348,7 +348,8 @@ class LTXEngine:
                         img.save(target_dest)
 
                 async with aiohttp.ClientSession() as download_session:
-                    tasks = [download_one(download_session, url, os.path.join(dynamic_guides_dir, f"guide_{i:04d}.png")) for i, enumerate in enumerate(urls_to_download)]
+                    # FIX: Corrected loop variables to (i, url) so 'url' is correctly defined
+                    tasks = [download_one(download_session, url, os.path.join(dynamic_guides_dir, f"guide_{i:04d}.png")) for i, url in enumerate(urls_to_download)]
                     await asyncio.gather(*tasks)
                 
                 image_filenames = [os.path.join(dynamic_guides_dir, f"guide_{i:04d}.png") for i in range(len(urls_to_download))]
@@ -446,7 +447,7 @@ class LTXEngine:
                         if "widgets_values" in sg2["252"]:
                             sg2["252"]["widgets_values"][0] = 4
 
-                    # ADDED CONFIGURATION: SET CFG Guider to 8 on Sub-graph 2
+                    # REVERTED CONFIGURATION: SET CFG Guider to 8 on Sub-graph 2
                     if "234" in sg2:
                         if "inputs" not in sg2["234"]: sg2["234"]["inputs"] = {}
                         sg2["234"]["inputs"]["cfg"] = 8
@@ -482,12 +483,14 @@ class LTXEngine:
                         if "inputs" not in sg2["248"]: sg2["248"]["inputs"] = {}
                         sg2["248"]["inputs"]["lora_name"] = target_detailer_lora
                         
-                    # ADDED CONFIGURATION: SET Steps count to 20 on Sub-graph 2
+                    # REVERTED CONFIGURATION: SET Steps count to 20 on Sub-graph 2
                     if "249" in sg2: 
                         if "inputs" not in sg2["249"]: sg2["249"]["inputs"] = {}
                         sg2["249"]["inputs"]["steps"] = 20
                         sg2["249"]["inputs"]["length"] = requested_length
                         sg2["249"]["inputs"]["max_frames"] = requested_length
+                        if "widgets_values" in sg2["249"] and len(sg2["249"]["widgets_values"]) > 0:
+                            sg2["249"]["widgets_values"][0] = 20
                     
                     if "233" in sg2:
                         if "inputs" not in sg2["233"]: sg2["233"]["inputs"] = {}

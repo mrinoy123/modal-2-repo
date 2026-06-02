955236652dbf64229d5589359be53c6fcd7c7083
Mrinoy Mitra <mrinoycool@gmail.com>
2026-06-02 16:26:15 +0600

    Update LTX-23-new.py

--- LTX-23-new.py
+++ LTX-23-new.py
@@ -29,7 +29,7 @@ base_image = modal.Image.from_registry(
     "build-essential", "ninja-build", "cmake", "clang", "llvm",
     "libgoogle-perftools-dev" 
 ).env({
-    "FORCE_REBUILD_INDEX": "221"  # Bumped to ensure a completely fresh image build layer
+    "FORCE_REBUILD_INDEX": "222"  # Bumped to ensure a completely fresh image build layer
 })
 
 # ==============================================================================
